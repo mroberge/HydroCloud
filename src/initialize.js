dd0a4a77d097f9caa40604b80b59cae34821e68e
mroberge <mroberge.whois@gmail.com>
2014-02-20 11:55:28 -0500

    Added points to map using real.kmz; added listener; click on points, info added to legend, data requested then plotted in graph. fixed alignment of button-bar; made brand pretty.

--- src/initialize.js
+++ src/initialize.js
@@ -10,6 +10,24 @@ function initialize() {
   //console.log(document.getElementById('map_div'));
   map = new google.maps.Map(document.getElementById('map_div'), mapOptions);
 
+        var points = new google.maps.KmlLayer("http://mroberge.github.io/real.kmz", {
+          suppressInfoWindows : true,
+          preserveViewport : true,
+          map : map
+        });
+
+        google.maps.event.addListener(points, 'click', function(kmlEvent) {
+          var sDesc = kmlEvent.featureData.description;
+          var sName = kmlEvent.featureData.name;
+          var re = /^[0-9]+/;
+          var sId = re.exec(sName);
+
+          $("#siteName").text(sName);
+          $("#siteQ").html(sDesc);
+          $("#siteId").text(sId);
+          getUSGS(sId[0]);
+        });
+
   tileNEX = new google.maps.ImageMapType({
     getTileUrl : function(tile, zoom) {
       return "http://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-900913/" + zoom + "/" + tile.x + "/" + tile.y + ".png?" + (new Date()).getTime();
