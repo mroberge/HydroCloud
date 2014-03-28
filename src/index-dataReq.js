f71c26627e14bbc678d31ab2eb4717291d95d66d
Martin Roberge <mroberge.whois@gmail.com>
2014-03-28 17:24:46 -0400

    changed data request to summer 2013 & did some clean up. System needs a state variable to keep track of data requests.

--- src/index-dataReq.js
+++
@@ -1,6 +0,0 @@
-function dataReq() {
-  console.log("dataReq");
-  var select = document.getElementById('selectRes');
-  var siteID = select.options[select.selectedIndex].value;
-  getUSGS(siteID);
-}
