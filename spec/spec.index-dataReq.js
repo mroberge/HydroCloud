d4ed92943577dce7bedd15e1749e14147c01941b
mroberge <mroberge.whois@gmail.com>
2014-05-28 12:26:13 -0400

    Clean up. removed unneeded files removed references to deleted files

--- spec/spec.index-dataReq.js
+++
@@ -1,8 +0,0 @@
-// spec sheet for dataReq.js
-
-describe("The dataReq.js file", function() {
-  it("contains an option-list reading function named dataReq()", function() {
-    expect(dataReq).toBeDefined();
-    expect(dataReq).toEqual(jasmine.any(Function));
-  });
-});
