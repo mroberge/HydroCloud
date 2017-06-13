8d26e80231f8328fd8fcbd511518e7b712f3284d
mroberge <mroberge.whois@gmail.com>
2017-06-12 21:32:22 -0400

    a start on a downloadJSON function

--- src/download.js
+++ src/download.js
@@ -19,3 +19,21 @@ function downloadCSV(options) {
     link.setAttribute('download', filename);
     link.click();
 }
+
+function downloadJSON(options) {
+    if (options === undefined || options === null) options = {};
+    var json, data, filename, link;
+
+    json = options.json;
+
+    if (json == null) return;
+
+    filename = options.filename || 'export.json';
+
+    data = encodeURI(json);
+
+    link = document.createElement('a');
+    link.setAttribute('href', data);
+    link.setAttribute('download', filename);
+    link.click();
+}
